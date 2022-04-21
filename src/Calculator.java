cf07354d752b96e5aa3dd71e61af7792b150cc9c
aimbotster <104161286+aimbotster@users.noreply.github.com>
2022-04-22 00:59:23 +0700

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+        public static void main(String[] args) {
+
+            Scanner input_data = new Scanner(System.in);
+
+            System.out.println("Enter your first number");
+            double num1 = input_data.nextDouble();
+            System.out.println("Enter your second number");
+            double num2 = input_data.nextDouble();
+            double Result = num1+num2;
+            System.out.println(Result);
+        }
+    }
+
