868e61774357395c2daa7ea218b19ed4bbe8c8e7
aimbotster <104161286+aimbotster@users.noreply.github.com>
2022-04-22 02:24:26 +0700

    Calculator 1

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,17 +1,57 @@
 import java.util.Scanner;
 
 public class Calculator {
+    public static void main(String[] args) {
 
-        public static void main(String[] args) {
+        Scanner input_data = new Scanner(System.in);
 
-            Scanner input_data = new Scanner(System.in);
 
-            System.out.println("Enter your first number");
-            double num1 = input_data.nextDouble();
-            System.out.println("Enter your second number");
-            double num2 = input_data.nextDouble();
-            double Result = num1*num2;
-            System.out.println(Result);
+        System.out.println("Enter your first number");
+        double num1 = input_data.nextDouble();
+
+        System.out.println("Select operation");
+        String operation = input_data.next();
+
+        System.out.println("Enter your second number");
+        double num2 = input_data.nextDouble();
+
+
+//            if (operation.equals("+")) {
+//                double result = num1 + num2;
+//                System.out.println(result);
+//            } else if (operation.equals("-")) {
+//                double result = num1 - num2;
+//                System.out.println(result);
+//            } else if (operation.equals("*")) {
+//                double result = num1 * num2;
+//                System.out.println(result);
+//            } else if (operation.equals("/")) {
+//                double result = num1 / num2;
+//                System.out.println(result);
+//            } else {
+//                System.out.println("Unidentified");
+//                System.exit(0);
+//            }
+
+
+        switch (operation) {
+            case "+" -> {
+                double result = num1 + num2;
+                System.out.println(result);
+            }
+            case "-" -> {
+                double result = num1 - num2;
+                System.out.println(result);
+            }
+            case "*" -> {
+                double result = num1 * num2;
+                System.out.println(result);
+            }
+            case "/" -> {
+                double result = num1 / num2;
+                System.out.println(result);
+            }
+
         }
     }
-
+}
