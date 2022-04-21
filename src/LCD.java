385a9edd4aa2ca9be1cce3c7576167b96a3d0b36
aimbotster <104161286+aimbotster@users.noreply.github.com>
2022-04-22 01:22:44 +0700

    Experiment 1.0

---
+++ src/LCD.java
@@ -0,0 +1,32 @@
+public class LCD {
+    public static void clearscreen(){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+}
